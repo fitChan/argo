64bd034077e450e958c8ba8a97eb2015d8e3d229
fitChan <84306157+fitChan@users.noreply.github.com>
2021-11-12 17:11:21 +0900

    OpenChatRoom is solved

--- src/main/java/com/myself/argo/programmers/lv2/OpenChatRoom.java
+++ src/main/java/com/myself/argo/programmers/lv2/OpenChatRoom.java
@@ -1,47 +1,44 @@
 package com.myself.argo.programmers.lv2;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class OpenChatRoom {
     public static String[] solution(String[] record) {
-        String[] answer = new String[record.length];
 
-
-        for (int i = record.length - 1; i >= 0; i--) {
-            if (record[i].split(" ")[0].equals("Change")) {
-                answer = new String[record.length-1];
-            }
-        }
-
-        ArrayList<String> uid = new ArrayList<>();
+        Map<String, String> uid = new HashMap<>();
         ArrayList<String> nickname = new ArrayList<>();
         for (int i = record.length - 1; i >= 0; i--) {
-            if (!uid.contains(record[i].split(" ")[1])) {
-                uid.add(record[i].split(" ")[1]);
-                if (record[i].split(" ").length == 3) {
-                    nickname.add(record[i].split(" ")[2]);
-                }
+            if (record[i].split(" ").length < 3) {
+                continue;
+            }
+            if (!uid.containsKey(record[i].split(" ")[1])) {
+                uid.put(record[i].split(" ")[1], record[i].split(" ")[2]);
             }
         }
 
+
         for (int i = 0; i < record.length; i++) {
-            for (int j = 0; j < uid.size(); j++) {
-                if (record[i].split(" ")[1].contains(uid.get(j))) {
-                    if (record[i].split(" ")[0].equals("Enter")) {
-                        answer[i] = "\""+nickname.get(j) + "님이 " + "들어왔습니다."+"\"";
-                    } else if (record[i].split(" ")[0].equals("Leave")) {
-                        answer[i] = "\""+nickname.get(j) + "님이 " + "나갔습니다."+"\"";
-                    } else {
-                        break;
-                    }
-                }
+            if (record[i].split(" ")[0].equals("Enter")) {
+                nickname.add(uid.get(record[i].split(" ")[1]) + "님이 들어왔습니다.");
+
+            } else if (record[i].split(" ")[0].equals("Leave")) {
+                nickname.add(uid.get(record[i].split(" ")[1]) + "님이 나갔습니다.");
             }
         }
-        for(String an :answer) {
-            System.out.print(an);
+        String[] answer = new String[nickname.size()];
+        int i=0;
+        for(String s : nickname){
+            answer[i]=s;
+            i++;
         }
+
+
+
         return answer;
     }
+
 
     public static void main(String[] args) {
         String[] record = {"Enter uid1234 Muzi", "Enter uid4567 Prodo", "Leave uid1234", "Enter uid1234 Prodo", "Change uid4567 Ryan"};
@@ -49,4 +46,5 @@ public class OpenChatRoom {
         solution(record);
 
     }
+
 }
