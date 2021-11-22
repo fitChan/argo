018b0914d449add8942f7d775d63ac781f7640dd
fitChan <84306157+fitChan@users.noreply.github.com>
2021-11-23 07:00:08 +0900

    A1463, A1932 is solved

---
+++ src/main/java/com/myself/argo/A1463.java
@@ -0,0 +1,21 @@
+package com.myself.argo;
+
+import java.util.Scanner;
+
+public class A1463 {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int x = sc.nextInt();
+        int[] arr = new int[10000001];
+
+        for(int i=2; i<=x; i++){
+            arr[i] = arr[i-1] +1;
+            if(i%2 == 0)
+                arr[i] = Math.min(arr[i], arr[i/2]+1);
+            if(i%3 == 0)
+                arr[i] = Math.min(arr[i], arr[i/3]+1);
+        }
+        System.out.println(arr[x]);
+    }
+}
--- src/main/java/com/myself/argo/A1932.java
+++ src/main/java/com/myself/argo/A1932.java
@@ -1,50 +1,37 @@
 package com.myself.argo;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
 import java.util.Scanner;
 
 public class A1932 {
     static int[][] arr;
-    static int nx = 0;
-    static int ny = 0;
-    static int sum = 0;
+    static Integer[][] dp;
+    static int n;
 
-    public static void solution(int n) {
-        Queue<int[]> q = new LinkedList<>();
-        int max = 0;
-        q.add(new int[]{0, 0});
-        sum = arr[0][0];
-        while (!q.isEmpty()) {
-            int[] nowNum = q.poll();
-            if (nx < n) {
-                nx++;
-                for (int i = 0; i < 2; i++) {
-                    ny = i + nowNum[1];
-                    q.add(new int[]{nx, ny});
-                }
-                sum += arr[nx][ny];
-                System.out.println(sum);
-            }
-            if (sum > max) {
-                max = sum;
-            }
+    public static int solution(int x, int y) {
+        if (x == n - 1)
+            return dp[x][y];
+
+        if (dp[x][y] == null) {
+            dp[x][y] = Math.max(solution(x + 1, y), solution(x + 1, y + 1)) + arr[x][y];
         }
-        System.out.println(max);
+        return dp[x][y];
     }
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        n = sc.nextInt();
         arr = new int[n][n];
+        dp = new Integer[n][n];
         for (int i = 0; i < n; i++) {
             for (int j = 0; j <= i; j++) {
                 arr[i][j] = sc.nextInt();
             }
         }
-        solution(n - 1);
+        for (int i = 0; i < n; i++) {
+            dp[n - 1][i] = arr[n - 1][i];
+        }
+
+        System.out.println(solution(0, 0));
 
 
     }
