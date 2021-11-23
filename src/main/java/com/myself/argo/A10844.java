4c74582019581436e7eab1cb942e541e171f523d
fitChan <84306157+fitChan@users.noreply.github.com>
2021-11-23 20:20:15 +0900

    A10844 is solved

---
+++ src/main/java/com/myself/argo/A10844.java
@@ -0,0 +1,43 @@
+package com.myself.argo;
+
+import java.util.Scanner;
+
+public class A10844 {
+    static Long[][] arr;
+    public static Long solution(int position, int number) {
+        if(position == 1){
+            return arr[position][number];
+        }
+
+        if(arr[position][number]==null){
+            if(number == 1){
+                arr[position][number] = solution(position-1, 1);
+            }else if(number == 9){
+                arr[position][number] = solution(position-1, 8);
+            }else{
+                arr[position][number] = solution(position-1, number+1)+solution(position-1, number-1);
+            }
+        }
+        return arr[position][number];
+
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+
+        arr= new Long[n+1][10];
+
+        for(int i=0; i<10; i++){
+            arr[1][i] = 1L;
+        }
+
+        Long result = 0L;
+
+        for(int i=1; i<=9; i++){
+            result += solution(n,i);
+        }
+        System.out.println(result%1000000000);
+
+    }
+}
