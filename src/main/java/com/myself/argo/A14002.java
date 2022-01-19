c0474f9a8ee964e3760cb80b1e30be75ec9ceac2
fitChan <84306157+fitChan@users.noreply.github.com>
2022-01-19 20:08:17 +0900

    A14002  is not solved

---
+++ src/main/java/com/myself/argo/A14002.java
@@ -0,0 +1,20 @@
+package com.myself.argo;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class A14002 {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int testCase = sc.nextInt();
+        int[] arr = new int[testCase];
+        for (int i = 0; i < testCase; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+
+    }
+}
